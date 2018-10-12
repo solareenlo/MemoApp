a08ad2f6eacc4dd54287e108b09f931f190c585c
solareenlo <solareenlo@gmail.com>
2018-10-12 17:58:43 +0900

    画面全体の構造化とAppbarのスタイリング

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import BodyText from './src/elements/BodyText';
-
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
-        <BodyText>Hi!</BodyText>
+
+        <View style={styles.appbar}>
+            <View>
+                <Text style={styles.appbarTitle}>MEMODASU</Text>
+            </View>
+        </View>
+
+        <View style={styles.memoList}>
+            <View style={styles.memoListItem}>
+                <Text style={styles.memoTitle}>アプリのアイディア</Text>
+                <Text style={styles.memoDate}>2018.10.10</Text>
+            </View>
+
+            <View style={styles.memoListItem}>
+                <Text style={styles.memoTitle}>アプリのアイディア</Text>
+                <Text style={styles.memoDate}>2018.10.10</Text>
+            </View>
+
+            <View style={styles.memoListItem}>
+                <Text style={styles.memoTitle}>アプリのアイディア</Text>
+                <Text style={styles.memoDate}>2018.10.10</Text>
+            </View>
+
+            <View style={styles.memoListItem}>
+                <Text style={styles.memoTitle}>アプリのアイディア</Text>
+                <Text style={styles.memoDate}>2018.10.10</Text>
+            </View>
+
+            <View style={styles.memoListItem}>
+                <Text style={styles.memoTitle}>アプリのアイディア</Text>
+                <Text style={styles.memoDate}>2018.10.10</Text>
+            </View>
+        </View>
       </View>
     );
   }
@@ -21,4 +50,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+    appbar: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 90,
+        paddingTop: 30,
+        backgroundColor: '#D8D8D8',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    appbarTitle: {
+        color: '#fff',
+        fontSize: 24,
+    },
 });
