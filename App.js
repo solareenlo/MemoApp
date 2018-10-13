c4359be6caf4b3c76ec2b80dc8db9f10a5ab5c97
solareenlo <solareenlo@gmail.com>
2018-10-13 11:55:41 +0900

    Appbarのコンポーネント化

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Appbar from './src/components/Appbar';
 import MemoList from './src/components/MemoList';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.appbar}>
-            <View>
-                <Text style={styles.appbarTitle}>MEMODASU</Text>
-            </View>
-        </View>
-
+        <Appbar />
         <MemoList />
-
         <View style={styles.memoAddButton}>
             <Text style={styles.memoAddButtonTitle}>+</Text>
         </View>
@@ -29,26 +25,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         paddingTop: 90,
         backgroundColor: '#FFFDF6',
-    },
-    appbar: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        height: 90,
-        paddingTop: 30,
-        backgroundColor: '#D8D8D8',
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {width: 0, height: 0},
-        shadowOpacity: 0.9,
-        shadowRadius: 3,
-        zIndex: 10,
-    },
-    appbarTitle: {
-        color: '#fff',
-        fontSize: 24,
     },
     memoAddButton: {
         position: 'absolute',
