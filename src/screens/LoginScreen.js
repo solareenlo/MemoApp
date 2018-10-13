a771e843da70a220683233c9d4df340a5f3cf9a5
solareenlo <solareenlo@gmail.com>
2018-10-13 20:04:54 +0900

    メンバー登録画面の構造化とスタイリング

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+
+class LoginScreen extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    ログイン
+                </Text>
+                <TextInput value="Email Address" style={styles.imput} />
+                <TextInput value="Password" style={styles.imput} />
+                <TouchableHighlight  onPress={()=>{}} style={styles.button} >
+                    <Text style={styles.buttonTitle}>送信</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        padding: 24,
+        backgroundColor: '#fff',
+    },
+    title:{
+        fontSize: 32,
+        alignSelf: 'center',
+        marginBottom: 24,
+    },
+    imput: {
+        backgroundColor: '#ddd',
+        height: 48,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: '#bbb',
+        padding: 8,
+    },
+    button: {
+        backgroundColor: '#ff5976',
+        height: 36,
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonTitle: {
+        color: '#fff',
+        fontSize: 18,
+    },
+});
+
+export default LoginScreen;
