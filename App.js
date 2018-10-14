8ae4cc6c839a28feea5b59b76ef5a08cbfcf36c0
solareenlo <solareenlo@gmail.com>
2018-10-14 22:21:58 +0900

    各スクリーンの登録

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
 import { createStackNavigator } from 'react-navigation';
 
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
 
 const App = createStackNavigator({
-    Home: {
-        screen: MemoListScreen,
-        navigationOptions: () => ({
-            headerTitle: 'MEMODASU',
-            headerStyle: {
-                backgroundColor: '#BBB',
-            },
-            headerTitleStyle: {
-                color: '#fff',
-            },
-        }),
-    },
+    MemoDetail: { screen: MemoDetailScreen },
+    Home: { screen: MemoListScreen },
+    MemoEdit: { screen: MemoEditScreen },
+    Login: { screen: LoginScreen },
+    Signup: { screen: SignupScreen },
+}, {
+    navigationOptions: () => ({
+        headerTitle: 'MEMODASU',
+        headerStyle: {
+            backgroundColor: '#BBB',
+        },
+        headerTitleStyle: {
+            color: '#fff',
+        },
+    }),
 });
 
 export default App;
